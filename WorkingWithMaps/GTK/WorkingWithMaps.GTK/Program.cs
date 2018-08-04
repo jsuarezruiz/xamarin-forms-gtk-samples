7a89cb9d0cb966020c70a221d38ad655a5fd548d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-08-04 13:30:37 +0200

    More samples

---
+++ WorkingWithMaps/GTK/WorkingWithMaps.GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps.GTK;
+using Xamarin.Forms.Platform.GTK;
+
+namespace WorkingWithMaps.GTK
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+            FormsMaps.Init();
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("WorkingWithMaps");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
---
+++ XamlSamples/XamlSamples/XamlSamples.GTK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace XamlSamples.GTK
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("XamlSamples");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
