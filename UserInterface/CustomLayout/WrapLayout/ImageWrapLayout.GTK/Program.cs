65964cf8730053f92fc4e9a59b0748c7250f4cf8
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-09-04 21:07:01 +0200

    More samples

---
+++ UserInterface/CustomLayout/WrapLayout/ImageWrapLayout.GTK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace ImageWrapLayout.GTK
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("ImageWrapLayout");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
